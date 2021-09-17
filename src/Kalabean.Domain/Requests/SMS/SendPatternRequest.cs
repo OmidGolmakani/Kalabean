4dbf391843e4e79d15b2b4a0cb393ef0cd1a27de
Omid Golmakani <OmidGolmakani@outlook.com>
2021-09-17 22:21:55 +0430

    add sms

---
+++ src/Kalabean.Domain/Requests/SMS/SendPatternRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalabean.Domain.Requests.SMS
+{
+    public class SendPatternRequest
+    {
+        public string op
+        {
+            get
+            {
+                return "patternV2";
+            }
+        }
+        public string user
+        {
+            get
+            {
+                return "kalabean";
+            }
+        }
+        public string pass
+        {
+            get
+            {
+                return "k9158990160";
+            }
+        }
+        public string fromNum
+        {
+            get
+            {
+                return "10009158990160";
+            }
+        }
+        public string toNum { get; set; }
+        public string patternCode
+        {
+            get
+            {
+                return "6302k8s915";
+            }
+        }
+        public InputData inputData { get; set; }
+        public string ConvertToJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ src/Kalabean.Infrastructure/Services/SMSService.cs
@@ -0,0 +1,30 @@
+﻿using Kalabean.Domain.Services;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalabean.Infrastructure.Services
+{
+    public class SMSService : ISMSService
+    {
+        public async Task<string> SendPattern(string Code, string ToNum)
+        {
+            Kalabean.Domain.Requests.SMS.SendPatternRequest smsRequest = new Domain.Requests.SMS.SendPatternRequest()
+            {
+                inputData = new Domain.Requests.SMS.InputData() { SMSText = Code },
+                toNum = ToNum
+            };
+            var client = new RestClient("http://188.0.240.110/api/select");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("undefined", smsRequest.ConvertToJson()
+                , ParameterType.RequestBody);
+            IRestResponse response = await client.ExecuteAsync(request);
+            return response.Content;
+        }
+    }
+}
